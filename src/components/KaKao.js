14eb3621104c848c94de80cf1f4eae63686c6ca8
strongcookdas <goodjav141592@gmail.com>
2023-12-10 23:20:01 +0900

    chore : 로고 변경

--- src/components/KaKao.js
+++ src/components/KaKao.js
@@ -9,31 +9,31 @@ function KaKao({ location, width, height }) {
       center: new kakao.maps.LatLng(
         location?.latitude || 37.29432708770308,
         location?.longitude || 126.99658273529194
-      ), //지도의 중심좌표.
-      level: 3, //지도의 레벨(확대, 축소 정도)
+      ),
+      level: 3,
     };
-    //지도 생성 및 객체 리턴
     const map = new kakao.maps.Map(container, options);
 
-    // 마커가 표시될 위치입니다
     const markerPosition = new kakao.maps.LatLng(
       location?.latitude || 37.29432708770308,
       location?.longitude || 126.99658273529194
     );
 
-    // 마커를 생성합니다
     const marker = new kakao.maps.Marker({
       position: markerPosition,
     });
 
-    // 마커가 지도 위에 표시되도록 설정합니다
     marker.setMap(map);
   }, [location]);
 
   return (
     <div
       id="map"
-      style={{ width: width, height: height, marginRight: "20px" }}
+      style={{
+        width: width,
+        height: height,
+        marginRight: "20px",
+      }}
     ></div>
   );
 }
--- src/screens/AdminLoginView.js
+++ src/screens/AdminLoginView.js
@@ -29,7 +29,7 @@ const AdminLoginView = () => {
 
   return (
     <div className="admin-login-container">
-      <img className="logo" src="logo.png" alt="logo" />
+      <img className="logo" src="globe-americas-solid.svg" alt="logo" />
       <h1 className="admin-login-title">Exchangers Admin</h1>
       <div className="admin-login-form">
         <input
